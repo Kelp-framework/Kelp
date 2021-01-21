ce319ba3adefcba792c4700c933efca11b733572
brtastic <brtastic.dev@gmail.com>
2021-01-21 07:29:20 +0100

    Documentation and a minor change to Kelp::Generator

--- lib/Kelp/Generator.pm
+++ lib/Kelp/Generator.pm
@@ -31,9 +31,12 @@ sub get_scenario_files {
 }
 
 sub get_template {
-    my ($self, $scenario, $vars) = @_;
-    $vars->{$_} // croak "variable `$_` is required"
-        for qw(name module_path module_file);
+    my ($self, $scenario, $name, %args) = @_;
+
+    my $vars = {'name' => $name, %args};
+    my @parts = split(/::/, $name);
+    $vars->{module_file} = pop @parts;
+    $vars->{module_path} = join('/', @parts);
 
     my @list = $self->get_scenario_files($scenario);
     croak "There's no generation template for $scenario"
@@ -66,4 +69,107 @@ sub get_template {
 
 1;
 
-# TODO: Document template plugin mechanism for module authors
+=pod
+
+=head1 NAME
+
+Kelp::Generator - Generation templates
+
+=head1 SYNOPSIS
+
+    use Kelp::Generator;
+
+    # get available templates
+    my @scenarios = Kelp::Generator->list_scenarios;
+
+    # get parsed files (ready to be saved)
+    my $files_aref = Kelp::Generator->get_template($scenario, 'App::Name');
+
+    for my $file (@$files_aref) {
+        my ($file_name, $file_contents) = @$file;
+    }
+
+=head1 DESCRIPTION
+
+This is a class for discovery and parsing of generation templates for Kelp. A
+generation template is a set of files that can be parsed using
+L<Template::Tiny> and inserted into a given directory. This class only handles
+the discovery and parsing of these templates. The Kelp script or custom script
+should handle saving them in a destination directory.
+
+=head1 TEMPLATE CREATION
+
+=head2 Discovery
+
+This class will look into a directory in its installation tree to discover
+available scenarios. The folder is C<Kelp/templates> by default and can be
+changed by changing the contents of package variable
+C<$Kelp::Generator::scenarios_dir>. This means that CPAN modules can add
+templates to L<Kelp/templates> and they will be discovered as long as they have
+been installed in the same root directory as Kelp without changing the contents
+of the package variable. Any template that can be discovered in the default
+directory will be usable in the Kelp script.
+
+=head2 Contents
+
+The directory structure of C<Kelp/templates> directory is as follows:
+
+    + templates
+    | + template_name
+      | - template
+      | - file1.pl.gen
+      | - NAME.pm.gen
+    | + another_template_name
+      | - template
+      | - file1.tt
+
+Each template directory must have a file named C<template>, which lists all the
+files in that template like this:
+
+    file1.pl.gen
+    NAME.pm.gen
+
+Any file that is not listed will not be used.
+
+=head2 Template files
+
+Each template file can contain L<Template> code:
+
+    My::App::Name eq [% name %]
+    Name eq [% module_file %]
+    My/App eq [% module_path %]
+
+It will be replaced accordingly, but only if the file ends with C<.gen>
+extension. This extension also allows template files not to be confused with
+real files, so should be used most of the time. The only case where the C<.gen>
+extension should not be used in when generating template files using the same
+syntax as L<Template>, because there's no way to tell which directives should
+not be interpreted right away.
+
+Files can also contain NAME, FILE and PATH in their name, which will be
+replaced by C<name>, C<module_file> and C<module_path>.
+
+=head1 INTERFACE
+
+=head2 Methods
+
+=head3 get_template
+
+    my $template_aref = $gen->get_template($template_name, $application_name, %more_vars);
+
+Finds and parses template with L<Template::Tiny>, returning an array reference of files:
+
+    ['file1.pl', 'contents'],
+    ['replaced_name.pm', 'contents'],
+    ...
+
+Filenames will have directories and C<.gen> suffix stripped and all
+placeholders replaced. File contents will be ready for saving.
+
+C<%more_vars> can be specified to insert more variables into the template.
+
+=head3 list_scenarios
+
+    my @templates = $gen->list_scenarios;
+
+Discovers and returns all the generation template names as a list.
